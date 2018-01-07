64f3999f5b350b031acb223462b92b128f2e7a71
Fendo181 <fendo181git@gmail.com>
2018-01-07 21:47:49 +0900

    p80 Swiftのfor文

--- swift/MyPlayground.playground/Contents.swift
+++ swift/MyPlayground.playground/Contents.swift
@@ -1,22 +1,3 @@
-print(1+1);
-print("hello wold!");
-
-print(1-1);
-print(1*2);
-print(20/4);
-print(100+23);
-
-print("地球の体積は")
-//コメント
-print(4/3*3.14*6378*6378*6378)
-
-var x = 3;
-print(x*4);
-x = 10;
-print(x*4);
-
-//定数
-let tax = 1.08;
-print(100*tax);
-
-
+for n in 1...9 {
+    print (3 * n)
+}
